7ba218821e509e8c1bdc31c56429e64a9c595e34
Misha Wagner <mishajw@gmail.com>
2017-12-07 16:23:00 +0000

    Add OpenCV and load in two images

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <image 1> <image 2>" << std::endl;
+        exit(1);
+    }
+
+    std::string image_path1 = argv[1];
+    std::string image_path2 = argv[2];
+
+    cv::Mat image1 = cv::imread(image_path1, CV_LOAD_IMAGE_COLOR);
+    cv::Mat image2 = cv::imread(image_path2, CV_LOAD_IMAGE_COLOR);
+
+    cv::imshow("Image 1", image1);
+    cv::waitKey(0);
+
+    cv::imshow("Image 2", image2);
+    cv::waitKey(0);
 }
